f4a77af595799e79fcc4f1a660d240b7f4238e26
murer <murer@dextra-sw.com>
2016-11-17 11:38:28 -0200

    init

--- modopz-core/src/main/java/com/murerz/dsopz/core/DOZHandler.java
+++ modopz-core/src/main/java/com/murerz/dsopz/core/DOZHandler.java
@@ -4,10 +4,6 @@ import java.io.Closeable;
 
 public abstract class DOZHandler<T> implements Closeable {
 
-	public DOZHandler(DOZConfig config) {
-		
-	}
-	
 	public abstract Class<T> spec();
 
 	public abstract void onCommand(T command);
