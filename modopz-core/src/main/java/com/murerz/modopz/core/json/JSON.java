761cc751ff7c731e84fd24335a4b9b475e073a2f
murer <murer@dextra-sw.com>
2016-11-22 16:51:40 -0200

    init

--- modopz-core/src/main/java/com/murerz/modopz/core/json/JSON.java
+++ modopz-core/src/main/java/com/murerz/modopz/core/json/JSON.java
@@ -1,16 +1,20 @@
 package com.murerz.modopz.core.json;
 
 import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
 
 public class JSON {
 
 	public static String stringify(Object obj) {
-		return new Gson().toJson(obj);
+		return gson().toJson(obj);
+	}
+
+	private static Gson gson() {
+		return new GsonBuilder().registerTypeHierarchyAdapter(byte[].class, new MOByteArrayBase64GsonAdapter()).create();
 	}
 
 	public static <T> T parse(String json, Class<T> clazz) {
-		Gson gson = new Gson();
-		return gson.fromJson(json, clazz);
+		return gson().fromJson(json, clazz);
 	}
 
 }
---
+++ modopz-core/src/main/java/com/murerz/modopz/core/json/MOByteArrayBase64GsonAdapter.java
@@ -0,0 +1,36 @@
+package com.murerz.modopz.core.json;
+
+import java.lang.reflect.Type;
+
+import com.google.gson.JsonArray;
+import com.google.gson.JsonDeserializationContext;
+import com.google.gson.JsonDeserializer;
+import com.google.gson.JsonElement;
+import com.google.gson.JsonNull;
+import com.google.gson.JsonParseException;
+import com.google.gson.JsonPrimitive;
+import com.google.gson.JsonSerializationContext;
+import com.google.gson.JsonSerializer;
+
+public class MOByteArrayBase64GsonAdapter implements JsonSerializer<byte[]>, JsonDeserializer<byte[]> {
+
+	public byte[] deserialize(JsonElement json, Type typeOfT, JsonDeserializationContext context)
+			throws JsonParseException {
+		JsonArray array = json.getAsJsonArray();
+		byte[] ret = new byte[array.size()];
+		int i= 0;
+		for (JsonElement n : array) {
+			ret[i++] = n.getAsByte();
+		}
+		return ret;
+	}
+
+	public JsonElement serialize(byte[] src, Type typeOfSrc, JsonSerializationContext context) {
+		JsonArray ret = new JsonArray();
+		ret.add(new JsonPrimitive(97L));
+		ret.add(new JsonPrimitive(98L));
+		ret.add(new JsonPrimitive(99L));
+		return ret;
+	}
+
+}
