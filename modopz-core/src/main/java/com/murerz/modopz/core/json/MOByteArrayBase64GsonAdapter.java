e4edfa8ef67bef271c8d60b9f5a86fa468bb5021
murer <murer@dextra-sw.com>
2016-11-23 09:14:17 -0200

    init

--- modopz-core/src/main/java/com/murerz/modopz/core/json/MOByteArrayBase64GsonAdapter.java
+++ modopz-core/src/main/java/com/murerz/modopz/core/json/MOByteArrayBase64GsonAdapter.java
@@ -6,7 +6,6 @@ import com.google.gson.JsonArray;
 import com.google.gson.JsonDeserializationContext;
 import com.google.gson.JsonDeserializer;
 import com.google.gson.JsonElement;
-import com.google.gson.JsonNull;
 import com.google.gson.JsonParseException;
 import com.google.gson.JsonPrimitive;
 import com.google.gson.JsonSerializationContext;
@@ -18,7 +17,7 @@ public class MOByteArrayBase64GsonAdapter implements JsonSerializer<byte[]>, Jso
 			throws JsonParseException {
 		JsonArray array = json.getAsJsonArray();
 		byte[] ret = new byte[array.size()];
-		int i= 0;
+		int i = 0;
 		for (JsonElement n : array) {
 			ret[i++] = n.getAsByte();
 		}
