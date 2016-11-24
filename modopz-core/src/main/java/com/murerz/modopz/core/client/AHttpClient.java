49446844b7bb02e5f025bba64045ddcd0d0187ca
murer <murer@dextra-sw.com>
2016-11-24 14:19:03 -0200

    init

---
+++ modopz-core/src/main/java/com/murerz/modopz/core/client/AHttpClient.java
@@ -0,0 +1,15 @@
+package com.murerz.modopz.core.client;
+
+import com.murerz.modopz.core.service.AService;
+
+public class AHttpClient implements AService {
+
+	public <T> T module(Class<T> spec) {
+		return null;
+	}
+
+	public AService prepare(String url) {
+		return this;
+	}
+
+}
