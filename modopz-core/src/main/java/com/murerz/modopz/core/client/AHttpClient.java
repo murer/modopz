95f166c6ba29831b457026d807c74680ed2a170a
murer <murer@dextra-sw.com>
2016-11-24 14:23:08 -0200

    init

--- modopz-core/src/main/java/com/murerz/modopz/core/client/AHttpClient.java
+++ modopz-core/src/main/java/com/murerz/modopz/core/client/AHttpClient.java
@@ -4,7 +4,10 @@ import com.murerz.modopz.core.service.AService;
 
 public class AHttpClient extends AClient {
 
+	private String url;
+
 	public AService prepare(String url) {
+		this.url = url;
 		return this;
 	}
 
