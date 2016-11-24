2da32f83136408ac26fd1d8a1610cdc3472c483e
murer <murer@dextra-sw.com>
2016-11-24 14:19:53 -0200

    init

--- modopz-core/src/main/java/com/murerz/modopz/core/client/AClient.java
+++ modopz-core/src/main/java/com/murerz/modopz/core/client/AClient.java
@@ -1,11 +1,10 @@
 package com.murerz.modopz.core.client;
 
-import com.murerz.modopz.core.service.AModule;
 import com.murerz.modopz.core.service.AService;
 
 public abstract class AClient implements AService {
 
-	public AModule module(String module) {
+	public <T> T module(Class<T> spec) {
 		return null;
 	}
 
--- modopz-core/src/main/java/com/murerz/modopz/core/client/AHttpClient.java
+++ modopz-core/src/main/java/com/murerz/modopz/core/client/AHttpClient.java
@@ -2,11 +2,7 @@ package com.murerz.modopz.core.client;
 
 import com.murerz.modopz.core.service.AService;
 
-public class AHttpClient implements AService {
-
-	public <T> T module(Class<T> spec) {
-		return null;
-	}
+public class AHttpClient extends AClient {
 
 	public AService prepare(String url) {
 		return this;
