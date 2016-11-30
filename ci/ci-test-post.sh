939b3accf7f016ef04b7b5c981b46c5c7949a913
murer <murer@dextra-sw.com>
2016-11-30 11:02:55 -0200

    init

--- ci/ci-test-post.sh
+++ ci/ci-test-post.sh
@@ -1,5 +1,5 @@
-#!/bin/bash -xe
+#!/bin/bash -x
 
 mkdir -p "$CIRCLE_ARTIFACTS/dist"
-cp modopz-server/target/modopz-server-dist-single.jar $CIRCLE_ARTIFACTS/dist || true
+cp modopz-server/target/modopz-server-dist-single.jar $CIRCLE_ARTIFACTS/dist
 du -hs $CIRCLE_ARTIFACTS/dist/*
